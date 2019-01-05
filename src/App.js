ac3bfe7267d2a45254e23530b423bda947201963
Bongani Mbigi <bongani@utexas.edu>
2019-01-05 04:36:19 -0600

    travis fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <h1 id='app'>Welcome to My World!!</h1>
+      <h1 id='app'>Welcome to My World!</h1>
     );
   }
 }
